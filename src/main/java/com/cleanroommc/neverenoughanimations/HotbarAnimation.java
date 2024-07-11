3f8371251d85ed3ac3799c05f85e22ca12b7f942
brachy84 <brachy84@gmail.com>
2024-07-11 22:52:33 +0200

    animate hotbar

--- src/main/java/com/cleanroommc/neverenoughanimations/HotbarAnimation.java
+++ src/main/java/com/cleanroommc/neverenoughanimations/HotbarAnimation.java
@@ -1,4 +1,59 @@
 package com.cleanroommc.neverenoughanimations;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+
 public class HotbarAnimation {
+
+    private static int oldIndex = -1, newIndex = -1;
+    private static int fromX = -1, currentX = -1;
+    private static long startTime = 0;
+
+    public static void animate(int oldIndex, int newIndex) {
+        if (isAnimationInProgress()) {
+            fromX = currentX;
+        } else {
+            HotbarAnimation.oldIndex = oldIndex;
+        }
+        HotbarAnimation.newIndex = newIndex;
+        if (HotbarAnimation.oldIndex == HotbarAnimation.newIndex) {
+            reset();
+            return;
+        }
+        HotbarAnimation.startTime = Minecraft.getSystemTime();
+    }
+
+    public static boolean isAnimationInProgress() {
+        return HotbarAnimation.oldIndex >= 0;
+    }
+
+    public static void reset() {
+        HotbarAnimation.oldIndex = -1;
+        HotbarAnimation.newIndex = -1;
+        HotbarAnimation.fromX = -1;
+        HotbarAnimation.currentX = -1;
+    }
+
+    public static int getX(ScaledResolution sr) {
+        int def = Minecraft.getMinecraft().player.inventory.currentItem;
+        if (oldIndex < 0 || newIndex < 0) return getX(sr, def);
+        if (def != newIndex) {
+            // index unexpectedly changed, abort animation
+            reset();
+            return getX(sr, def);
+        }
+        float val = (Minecraft.getSystemTime() - HotbarAnimation.startTime) / (float) NEAConfig.hotbarAnimationTime;
+        if (val >= 1f) {
+            // animation ended
+            reset();
+            return getX(sr, def);
+        }
+        if (fromX < 0) fromX = getX(sr, oldIndex);
+        currentX = (int) NEAConfig.hotbarAnimationCurve.interpolate(fromX, getX(sr, newIndex), val);
+        return currentX;
+    }
+
+    public static int getX(ScaledResolution sr, int index) {
+        return sr.getScaledWidth() / 2 - 91 - 1 + index * 20;
+    }
 }
--- src/main/java/com/cleanroommc/neverenoughanimations/NEAConfig.java
+++ src/main/java/com/cleanroommc/neverenoughanimations/NEAConfig.java
@@ -6,6 +6,7 @@ import net.minecraftforge.common.config.Config;
 @Config(modid = Tags.MODID)
 public class NEAConfig {
 
+    @Config.SlidingOption
     @Config.RangeInt(min = 0, max = 2000)
     @Config.Comment("How many millieseconds it takes until an item is scaled to its full size on hover. 0 to disable.")
     public static int hoverAnimationTime = 250;
@@ -15,9 +16,16 @@ public class NEAConfig {
     @Config.Comment("If the gray slot overlay should be rendered at all on hover.")
     public static boolean itemHoverOverlay = false;
 
+    @Config.SlidingOption
+    @Config.RangeInt(min = 0, max = 2000)
     @Config.Comment("How many millieseconds it takes until an item has moved to its target. 0 to disable.")
     public static int moveAnimationTime = 500;
 
     public static Interpolation moveAnimationCurve = Interpolation.QUART_OUT;
 
+    @Config.SlidingOption
+    @Config.RangeInt(min = 0, max = 2000)
+    @Config.Comment("How many millieseconds it takes until the current item marker in the hotbar moved to its new location. 0 to disable.")
+    public static int hotbarAnimationTime = 100;
+    public static Interpolation hotbarAnimationCurve = Interpolation.QUAD_INOUT;
 }
