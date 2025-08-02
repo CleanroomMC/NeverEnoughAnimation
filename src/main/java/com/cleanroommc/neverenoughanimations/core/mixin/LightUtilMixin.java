db357aaf9d19c0f79a793ef671433ae48e5f281d
brachy84 <brachy84@gmail.com>
2025-08-02 14:42:00 +0200

    fix colored items showing colorless during animation

--- src/main/java/com/cleanroommc/neverenoughanimations/core/mixin/LightUtilMixin.java
+++ src/main/java/com/cleanroommc/neverenoughanimations/core/mixin/LightUtilMixin.java
@@ -3,21 +3,22 @@ package com.cleanroommc.neverenoughanimations.core.mixin;
 import com.cleanroommc.neverenoughanimations.NEA;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
-import net.minecraft.client.renderer.RenderItem;
-import net.minecraft.client.renderer.block.model.IBakedModel;
-import net.minecraft.item.ItemStack;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraftforge.client.model.pipeline.LightUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(RenderItem.class)
-public class RenderItemMixin {
+@Mixin(LightUtil.class)
+public class LightUtilMixin {
 
-    @Inject(method = "renderModel(Lnet/minecraft/client/renderer/block/model/IBakedModel;ILnet/minecraft/item/ItemStack;)V", at = @At("HEAD"))
-    public void renderItem(IBakedModel model, int color, ItemStack stack, CallbackInfo ci, @Local(argsOnly = true, ordinal = 0) LocalIntRef colorRef) {
+    @Inject(method = "renderQuadColor", at = @At("HEAD"), remap = false)
+    private static void injectItemAlpha(BufferBuilder buffer, BakedQuad quad, int auxColor, CallbackInfo ci, @Local(argsOnly = true, ordinal = 0) LocalIntRef colorRef) {
         if (NEA.isCurrentGuiAnimating()) {
             colorRef.set(NEA.withAlpha(colorRef.get(), (int) (NEA.getAlpha(colorRef.get()) * NEA.getCurrentOpenAnimationValue())));
         }
     }
+
 }
